73be46d5e43edddab7b77c40c012295ffca8801a
DeveloperViraj <workwithvirajjadhav@gmail.com>
2025-09-01 02:48:56 +0530

    Dasboard changes 5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./styles.css";
 import App from "./App";
+
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
