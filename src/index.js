fa8c3fe35b563b1a5b3b68f1a2226e09beba1225
DeveloperViraj <workwithvirajjadhav@gmail.com>
2026-01-22 22:31:11 +0530

    Add Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "antd/dist/reset.css";
 import "./App.css";
-import "./antd-overrides.css"; 
+import "./antd-overrides.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -15,5 +15,6 @@ root.render(
   <React.StrictMode>
     <ToastContainer />
     <App />
+    <Analytics />
   </React.StrictMode>
 );
